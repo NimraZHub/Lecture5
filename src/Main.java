b37431071b80995a657bd60b4005c34c1af68f34
NimraZHub <muhammadasifasiftaj@gmail.com>
2024-08-05 11:48:35 +0500

    second

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        //Function
+        print();
+
+    }
+    public static void print(){
+        System.out.println("Message 1");
+        print1();
+    }
+
+    private static void print1() {
+        System.out.println("Message 2");
+        print2();
+    }
+
+    private static void print2() {
+        System.out.println("Message 3");
     }
 }
